32ec0641ed1feed55c2fd3b0f165e95cb5f2c275
root <root@localhost.localdomain>
2018-02-07 13:32:34 +0700

    add install_nginx.sh

---
+++ install_nginx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#install epel-release
+yum install -y epel-release
+
+#install nginx
+yum install -y nginx
+
+#start and enable nginx
+systemctl start nginx
+systemctl enable nginx
+
+#creat firewall http
+firewall-cmd --permanent --zone=public --add-service=http
+
+#https
+firewall-cmd --permanent --zone=public --add-service=https
+
+#reload
+firewall-cmd --reload
+
+
